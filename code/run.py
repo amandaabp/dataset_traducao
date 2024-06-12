32ca30f0c5d3b795fef3ea6f8b652e1d6279aa7b
amandaabp <58114420+amandaabp@users.noreply.github.com>
2024-06-12 20:31:47 -0300

    Update run

--- code/run.py
+++ code/run.py
@@ -79,7 +79,7 @@ def main(args):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(argument_default=argparse.SUPPRESS, description="End-to-end AI Question/Answer example for gen-ai")
-    parser.add_argument('-m', 'Phi-3-mini-4k-instruct-onnx', type=str, required=True, help='Onnx model folder path (must contain config.json and model.onnx)')
+    parser.add_argument('-m', '--Phi-3-mini-4k-instruct-onnx', type=str, required=True, help='Onnx model folder path (must contain config.json and model.onnx)')
     ''''
     parser.add_argument('-i', '--min_length', type=int, help='Min number of tokens to generate including the prompt')
     parser.add_argument('-l', '--max_length', type=int, help='Max number of tokens to generate including the prompt')
