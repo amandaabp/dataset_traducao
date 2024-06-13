39cc73a12a625f307865d8c721b97c0092450c31
amandaabp <58114420+amandaabp@users.noreply.github.com>
2024-06-12 21:17:45 -0300

    Update run

--- code/run.py
+++ code/run.py
@@ -79,14 +79,14 @@ def main(args):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(argument_default=argparse.SUPPRESS, description="End-to-end AI Question/Answer example for gen-ai")
-    
+    parser.add_argument('-m', 'cuda/cuda-int4-rtn-block-32', type=str, required=True, help='Onnx model folder path (must contain config.json and model.onnx)')
     parser.add_argument('-v', '--verbose', action='store_true', default=False, help='Print verbose output and timing information. Defaults to false')
     parser.add_argument('-g', '--timings', action='store_true', default=False, help='Print timing information for each generation step. Defaults to false')
     args = parser.parse_args()
     main(args)
 
     ''''
-    parser.add_argument('-m', 'cuda/cuda-int4-rtn-block-32', type=str, required=True, help='Onnx model folder path (must contain config.json and model.onnx)')
+    
     parser.add_argument('-i', '--min_length', type=int, help='Min number of tokens to generate including the prompt')
     parser.add_argument('-l', '--max_length', type=int, help='Max number of tokens to generate including the prompt')
     parser.add_argument('-ds', '--do_sample', action='store_true', default=False, help='Do random sampling. When false, greedy or beam search are used to generate the output. Defaults to false')
