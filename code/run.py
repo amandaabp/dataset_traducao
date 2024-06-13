b3e09f8ac5b6c369e766a2b3f5fc02c04365d4e8
pa-tiq <patrickdsilva99@gmail.com>
2024-06-13 19:13:37 -0300

    stdin_open

--- code/run.py
+++ code/run.py
@@ -1,15 +1,12 @@
-import os
-print(os.environ.get('CUDA_PATH'))
-
 import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
 
 torch.random.manual_seed(0)
 model_id = "microsoft/Phi-3-small-8k-instruct"
 model = AutoModelForCausalLM.from_pretrained(
-    model_id, 
-    torch_dtype="auto", 
-    trust_remote_code=True, 
+    model_id,
+    torch_dtype="auto",
+    trust_remote_code=True,
 )
 
 if torch.cuda.is_available():
@@ -20,17 +17,18 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 tokenizer = AutoTokenizer.from_pretrained(model_id)
 
 messages = [
-    {"role": "user", "content": "Can you provide ways to eat combinations of bananas and dragonfruits?"},
-    {"role": "assistant", "content": "Sure! Here are some ways to eat bananas and dragonfruits together: 1. Banana and dragonfruit smoothie: Blend bananas and dragonfruits together with some milk and honey. 2. Banana and dragonfruit salad: Mix sliced bananas and dragonfruits together with some lemon juice and honey."},
+    {
+        "role": "user",
+        "content": "Can you provide ways to eat combinations of bananas and dragonfruits?",
+    },
+    {
+        "role": "assistant",
+        "content": "Sure! Here are some ways to eat bananas and dragonfruits together: 1. Banana and dragonfruit smoothie: Blend bananas and dragonfruits together with some milk and honey. 2. Banana and dragonfruit salad: Mix sliced bananas and dragonfruits together with some lemon juice and honey.",
+    },
     {"role": "user", "content": "What about solving an 2x + 3 = 7 equation?"},
 ]
 
-pipe = pipeline(
-    "text-generation",
-    model=model,
-    tokenizer=tokenizer,
-    device=device
-)
+pipe = pipeline("text-generation", model=model, tokenizer=tokenizer, device=device)
 
 generation_args = {
     "max_new_tokens": 500,
@@ -41,7 +39,8 @@ generation_args = {
 
 output = pipe(messages, **generation_args)
 import json
-with open('data.json', 'w') as f:
+
+with open("data.json", "w") as f:
     json.dump(output, f)
 
-print(output[0]['generated_text'])
+print(output[0]["generated_text"])
